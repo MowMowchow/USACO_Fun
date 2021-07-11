2247871639caf7a3147a9c7385c4686c193bb639
MowMowChow <darkstar.hou2@gmail.com>
2021-07-11 17:03:28 -0400

    initial commit

--- silver/USACO17S2_hoofPaperScissors.cpp
+++ silver/USACO17S2_hoofPaperScissors.cpp
@@ -13,10 +13,35 @@ void setIO(string s) {
 	freopen((s+".out").c_str(),"w",stdout);
 }
 
-int main(){
-	// setIO("hps");
+int N, final = 0, H[100100], P[100100], S[100100];
+string a;
 
+int main(){
+	setIO("hps");
+	memset(H, 0, sizeof(H));
+	memset(P, 0, sizeof(P));
+	memset(S, 0, sizeof(S));
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++){
+		cin >> a;
+		if (a == "P") H[i]++;
+		else if (a == "S") P[i]++;	
+		else S[i]++;
+	}
   
+	for (int i = 1; i <= N; i++) {
+		H[i] += H[i-1];
+		S[i] += S[i-1];
+		P[i] += P[i-1];
+	}
+
+	for (int i = 1; i < N; i++){
+		final = max(final, max(H[i], max(S[i], P[i])) + max(H[N]-H[i], max(S[N]-S[i], P[N]-P[i])));
+	}
+
+	cout << final << "\n";
 
   return 0;
 }

7b0581848885884f49e6d7148b4c8d7f30b5e929
MowMowChow <darkstar.hou2@gmail.com>
2021-07-24 00:10:51 -0400

    initial commit

--- silver/USACO18S2_rentalService.cpp
+++ silver/USACO18S2_rentalService.cpp
@@ -13,12 +13,15 @@ void setIO(string s) {
 	freopen((s+".out").c_str(),"w",stdout);
 }
 
-ll N, M, R, a, b, curr = 0, final = 0;
-vec1ll rent, cows;
+ll N, M, R, a, b, bArr[100010], final = 0, rent[100010];
+vec1ll cows;
 deque<vec1ll> buy;
 
 int main(){
-	setIO("rental");
+	// setIO("rental");
+
+  memset(bArr, 0, sizeof(bArr));
+  memset(rent, 0, sizeof(rent));
 
   cin >> N >> M >> R;
   for (int i = 0; i < N; i++){
@@ -30,31 +33,47 @@ int main(){
     buy.push_back({a, b});
   }
   for (int i = 0; i < R; i++){
-    cin >> a;
-    rent.push_back(a);
+    cin >> rent[i];
   }
 
   sort(cows.begin(), cows.end(), [](const ll&x, const ll&y){return x > y;});
-  sort(rent.begin(), rent.end(), [](const ll&x, const ll&y){return x > y;});
+  sort(rent, rent + R);
   sort(buy.begin(), buy.end(), [](const vec1ll&v1, const vec1ll&v2){return v1[1] > v2[1];});
 
-  for (int i = 1; i < R; i++) rent[i] += rent[i-1];
-
-  for (int i = 0; i < N; i++){
-    while (cows[i] && !buy.empty()){
-      if (cows[i] >= buy[0][0]){
-        curr += buy[0][0]*buy[0][1];
-        cows[i] -= buy[0][0];
-        buy.pop_front(); 
-      } else if (cows[i] < buy[0][0]){
-        curr += cows[i]*buy[0][1];
-        buy[0][0] -= cows[i];
-        cows[i] = 0;
-      }
+  for (int i = R-2; i >= 0; i--) rent[i] += rent[i+1];
+  
+  for (int i = 1; i <= N; i++){
+    while (cows[i-1] <= 0 || !buy.empty()){
+      if (cows[i-1] >= buy[0][0]){ // bought all
+        cows[i-1] -= buy[0][0];
+        bArr[i] += buy[0][0]*buy[0][1];
+        buy.pop_front();
+      } else if (cows[i-1] < buy[0][0]){
+        buy[0][0] -= cows[i-1];
+        bArr[i] += cows[i-1]*buy[0][1];
+        break;
+      } 
+      if (cows[i-1] <= 0) break;
     }
-    int j = R-i-1 > -1 ? R-i-1 : 0;
-    final = max(final, curr+rent[j]);
-  } 
+    bArr[i] += bArr[i-1];
+  }
+
+  int diff = max(N-R, (ll) 0);
+
+  // cout << "~~~~~~~~~~~~~~~~~~~~~\n";
+
+  // for (int i = 0; i <= N; i++) cout << bArr[i] << " "; cout << "\n";
+  // for (int i = 0; i <= N; i++) cout << rent[i] << " "; cout << "\n";
+  
+  for (int i = 0; i <= N; i++)
+    if (i >= diff) bArr[i] += rent[i-diff];
+
+
+  for (int i = 0; i <= N; i++) final = max(final, bArr[i]);
+  
+  // for (int i = 0; i <= N; i++) cout << bArr[i] << " "; cout << "\n";
+
+
 
   cout << final << "\n";
 

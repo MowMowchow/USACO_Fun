6ed72cb404fc1751139eb7a41b75c365d7f2e4fa
MowMowChow <darkstar.hou2@gmail.com>
2021-08-08 18:09:36 -0400

    initial commit

--- silver/USACO16S2_diamondCollector.cpp
+++ silver/USACO16S2_diamondCollector.cpp
@@ -13,9 +13,23 @@ void setIO(string s) {
 	freopen((s+".out").c_str(),"w",stdout);
 }
 
-ll N, K, a, final = 0;
+ll N, K, a, final = 0, NR;
 vec1ll arr, ans;
+vec2ll ranges;
 
+
+int bs(ll x){
+  ll l = 0, r = NR-1, mid, ans = -1;
+  while(l <= r){
+    mid = l+(r-l)/2;
+    if (ranges[mid][0] <= x) l = mid+1;
+    else{
+      ans = mid;
+      r = mid-1;
+    }
+  }
+  return ans;
+}
 
 int main(){
 	setIO("diamond");
@@ -32,20 +46,33 @@ int main(){
 
   while (l <= r){
     if (arr[r]-arr[l] <= K){
-      r++;
-      if (r == N-1){
-        ans.push_back(r-l);
-        break;
+      if (r < N-1) r++;
+      else {
+        ranges.push_back({l, r-1, r-l});
+        l++;
       }
     } else {
-      ans.push_back({r-l});
-      l = r;
+      if (r > 0)
+        if (arr[r-1]-arr[l] <= K)
+          ranges.push_back({l, r-1, r-l});
+      l++;
     }
+    if (l == N-1) break;
   }
 
-  sort(ans.begin(), ans.end(), [](const ll&a, const ll&b){return a > b;});
+  NR = ranges.size();
 
-  cout << ans[0]+ans[1] << "\n";
+  sort(ranges.begin(), ranges.end());
+
+  for (int i = NR-2; i >= 0; i--) ranges[i][2] = max(ranges[i][2], ranges[i+1][2]);
+
+  for (int i = 0; i < NR; i++){
+    ll res = bs(ranges[i][1]);
+    if (res != -1)
+      final = max(final, (ranges[i][1]-ranges[i][0]+1)+ranges[res][2]);
+  }
+
+  cout << final << "\n";
 
   return 0;
 }

0bd20890604a97d1a9555721d03fbfcdee531c17
MowMowChow <darkstar.hou2@gmail.com>
2021-08-15 17:15:45 -0400

    initial commit

--- silver/USACO18S2_conventionII.cpp
+++ silver/USACO18S2_conventionII.cpp
@@ -13,29 +13,54 @@ void setIO(string s) {
 	freopen((s+".out").c_str(),"w",stdout);
 }
 
-ll N, a, t, T = 0, w = 0;
+ll N, a, t, T = 0, w = 0, cowsProcessed = 0, k = 0;
 vec2ll cows;
+map<ll, int> cowsHit;
+priority_queue<vec1ll, vec2ll, greater<vec1ll>> wq;
 
 int main(){
-	// setIO("convention2");
+	setIO("convention2");
 
   cin >> N;
   for (int i = 0; i < N; i++){
     cin >> a >> t;
-    cows.push_back({a, t, i}); // finishes at a+t inclusive -> next goes at a+t+1
+    cows.push_back({a, t, i+1});
   }
 
   sort(cows.begin(), cows.end(), [](const vec1ll&v1, const vec1ll&v2){return v1[0]==v2[0]?v1[2] < v2[2]:v1[0]<v2[0];});
-  // have a waiting queue and each time you use a cow update the queue
-  
-  for (auto c: cows){
-    if (c[0] > T){ // if the cow does not have to wait
-      T = c[0]+c[1]; // set to next avail time
-    } else { // if the cow has to wait
-      w += T-c[0]; 
-      T += c[1];
+
+  while (cowsProcessed < N){
+    if (!wq.empty()){
+      cowsProcessed++;
+      vec1ll curr = wq.top();
+      wq.pop();
+      cowsHit[curr[0]] = 1;
+      if (curr[1] > T) {
+        T = curr[1] + curr[2];
+      } else {
+        w = max(w, T-curr[1]);
+        T += curr[2];
+      }
+      while (k < N && cows[k][0] <= T){
+        wq.push({cows[k][2], cows[k][0], cows[k][1]});
+        k++;
+      }
+    } else if (k < N && !cowsHit[cows[k][2]]){
+      cowsHit[cows[k][2]] = 1;
+      cowsProcessed++;
+      if (cows[k][0] > T){ 
+        T = cows[k][0] + cows[k][1];
+        k++;
+      } else {
+        w = max(w, T-cows[k][0]);
+        T += cows[k][1];
+        k++;
+      }
+      while (k < N && cows[k][0] <= T){
+        wq.push({cows[k][2], cows[k][0], cows[k][1]});
+        k++;
+      }
     }
-    cout << "checking cow: " << c[2] << " spending: " << c[0] << " to " << c[1] << " | T: " << T << " | w: " << w <<  "\n";
   }
 
   cout << w << "\n";

9108ea696113f05c8965f313f0e0153b9d3367dd
MowMowchow <darkstar.hou2@gmail.com>
2022-02-19 09:33:20 -0500

    initial commit

--- silver/CSES_stickDivisions.cpp
+++ silver/CSES_stickDivisions.cpp
@@ -8,24 +8,21 @@ typedef vector<ll> vec1ll;
 typedef vector<vector<ll>> vec2ll;
 typedef vector<vector<vector<ll>>> vec3ll;
 
-ll N, x, a, ans = 0;
-vector<ll> arr;
+ll N, x, a, b, ans = 0;
+priority_queue<ll, vector<ll>, greater<ll>> pq;
 
 int main(){
-  cin >> N >> x;
-  for (int i = 0; i < x; i++){
+  cin >> x >> N;
+  for (ll i = 0; i < N; i++) {
     cin >> a;
-    arr.push_back(a);
+    pq.push(a);
   }
 
-  sort(arr.begin(), arr.end(), [](const ll&x, const ll&y){return x > y;});
-
-  for (int i = 0; i < x; i++){
-    if (i < x-1) {
-      cout << "adding | N: " << N << " | ans: " << ans << "\n";
-      ans += N;
-    }
-    N -= arr[i];
+  while (pq.size() > 1){
+    a = pq.top(); pq.pop();
+    b = pq.top(); pq.pop();
+    ans += a+b;
+    pq.push(a+b);
   }
 
   cout << ans << "\n";
